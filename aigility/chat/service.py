3fbe42a117274da5b3fa7d7f483b87d5b7b423ca
钱永岱 <qyd@qianyongdaideMacBook-Air.local>
2026-02-09 21:31:22 +0800

    【修复】修复对timem的rag检索服务响应数据的处理逻辑

--- aigility/chat/service.py
+++ aigility/chat/service.py
@@ -31,7 +31,7 @@ class ChatService:
             包含 AI 回复、建议和流程信息的响应对象。
         """
         session_id = request.session_id if request.session_id else str(uuid.uuid4())
-        timem_kb_id = request.kb_id if request.kb_id else "kb_default"
+        timem_kb_id = request.kb_id if request.kb_id else "kb_bb6a7f70f63a"
         config = RunnableConfig(
         configurable={
             "timem_kb_id": timem_kb_id # 这里拿到了"商家对应在太忆云的KB ID"
@@ -89,7 +89,7 @@ class ChatService:
         """
         session_id = request.session_id if request.session_id else str(uuid.uuid4())
         history = []
-        timem_kb_id = request.kb_id if request.kb_id else "kb_default"
+        timem_kb_id = request.kb_id if request.kb_id else "kb_bb6a7f70f63a"
         config = RunnableConfig(
         configurable={
             "timem_kb_id": timem_kb_id # 这里拿到了"商家对应在太忆云的KB ID"
